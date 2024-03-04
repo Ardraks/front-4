df092a48246575748d11eb5b94ab96f4a8a8b1d7
Ardraks <127195188+Ardraks@users.noreply.github.com>
2024-03-04 12:04:51 +0530

    Add files via upload

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+import "./Sidebar.css";
+import { useState } from "react";
+import baseurl from "../Api";
+
+const Sidebar = () => {
+  
+  
+  
+  
+    return (
+    <div className="sidebar">
+      <div className="sidebarItem">
+        <span className="sidebarTitle">ABOUT ME</span>
+        
+      </div>
+      
+    </div>
+  );
+}
+
+
+export default Sidebar
